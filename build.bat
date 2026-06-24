ef417b73a960683abff5ea7b6289b6d5cad7535f
Luca Saccarola <github.e41mv@aleeas.com>
2026-06-24 14:42:07 +0200

    Fix build.bat

--- build.bat
+++ build.bat
@@ -5,16 +5,12 @@ set prev_cwd=%cd%
 rem Make sure we are at the root of the project
 cd /D "%~dp0"
 
-set root=%cd%
 set src_root=%cd%\code
 set custom_root=%src_root%\custom
 set build_root=%cd%\build
 
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-    call %custom_root%\bin\setup_cl_x64.bat
-) else (
-    call %custom_root%\bin\setup_cl_x86.bat
-)
+rem If the user want's 32 bits they need to setup MSVC before calling the script.
+call %custom_root%\bin\setup_cl_x64.bat
 
 set opts=/nologo /FC /Zi /I%src_root% /I%custom_root%
 
@@ -23,7 +19,7 @@ pushd %build_root%
 call cl %opts% %src_root%\4ed_build.cpp /Febuild
 popd
 
-if %ERRORLEVEL% neq 0 exit /b
+if %ERRORLEVEL% neq 0 goto END
 
 %build_root%\build.exe %*
 
